131f38a3f6681711a6739f9c626ccb29fb4dfd9c
xerhaxs <xerhaxs@protonmail.com>
2023-06-07 14:54:13 +0200

    Update

--- scripts/testbase.sh
+++ scripts/testbase.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 
+FILE=https://raw.githubusercontent.com/xerhaxs/archInstaller/master/scripts
+
 echo "This is the first script"
-sudo pacman -S - < baum/test1
+sudo pacman -S - < $FILE/baum/test1
